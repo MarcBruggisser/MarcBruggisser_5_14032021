397e943cc158fe567f80fd27c24fe8cfb5b22a12
Marc Bruggisser <mb.bruggisser.projets@gmail.com>
2021-04-19 19:01:05 +0200

    V0.5 Page panier opérationnelle

--- assets/javascript/page_panier.js
+++ assets/javascript/page_panier.js
@@ -1,52 +1,101 @@
 /* Sur la page panier */
 if(main.classList.contains('page-panier')){
-  
-  
-  /* Indiquer un message si le panier est vide : */
-  
-  /* On récupère l'élément section */
-  let section = document.querySelector('section');
-  /* On vérifie que l'élément ait des éléments enfants (retourne une valeur booléene) */
-  let sectionHasChildren = section.hasChildNodes();
-  /* Si le panier ne possède aucun produit, alors on n'indique au client que le panier n'en possède pas */
-  if(sectionHasChildren === false){
+
+  function creerParagrapheErreur() {
     let paragrapheErreur = document.createElement("p");
     paragrapheErreur.classList.add('paragraphe-erreur');  
     paragrapheErreur.textContent = "Il n'y a pour l'instant aucun produit à l'intérieur de votre panier.";
     section.appendChild(paragrapheErreur);
   }
 
+  /* Si la section ne contient pas de produit, alors on empêche l'envoie du formulaire */
+  function empecherEnvoiFormulaire() {
+    let boutonFormulaireEnvoi = document.querySelector(".submit");
+    let inputsFormulaire = document.querySelectorAll("form input");
+    for (all of inputsFormulaire) {
+      all.style.background = "#C0C0C0";
+      all.setAttribute("disabled", "disabled");
+    }
+    boutonFormulaireEnvoi.addEventListener("click", function (event){
+      event.preventDefault();
+    });
+  }
+
+  /* On récupère l'élément section */
+  let section = document.querySelector('section');
+  
   /* Récupérer les données présentes à l'intérieur du localStorage */
   let panier = JSON.parse(localStorage.getItem("panier"));
-  /* Pour chaque objet présentà l'intérieur du localStorage */
-  for (all of panier){
-    /* On crée un nouvel article */
-    let newArticle = document.createElement("article");
-    /* On crée une balise pour y stocker le nom de l'article */
-    let nom = document.createElement("h2");
-    /* On attribue la valeur de la propriété "name" de l'objet en tant que textContent de la balise que l'on vient de créer */
-    nom.textContent = all.name;
-    /* On place le titre à l'intérieur de notre article */
-    newArticle.appendChild(nom);
-    /* On crée un select HTML pour que l'utilisateur puisse choisir la quantité d'appareil qu'il veut */
-    // document.createElement("select")
-    /* On crée une balise pour y placer le prix */
-    let prix = document.createElement("span");
-    /* On attribue la valeur de la propriété "price" de l'objet en tant que textContent de la balise que l'on vient de créer */
-    prix.textContent = all.price + " €";
-    /* On place le titre à l'intérieur de notre article */
-    newArticle.appendChild(prix);
-    /* On place notre article à l'intérieur de notre section */
-    section.appendChild(newArticle);
+
+  /* Si il y a des valeurs à l'intérieur du panier : pour chaque objet présent à l'intérieur du localStorage */
+  if(panier != null){
+    for (all of panier){
+      /* On crée un nouvel article */
+      let newArticle = document.createElement("article");
+      /* On crée une balise pour y stocker le nom de l'article */
+      newArticle.innerHTML = `<h2>${all.name}</h2><span class="montant">${all.price} €</span><input class="quantite" type="number" value="1" /><span class="total">${all.price} €</span><button class="supprimer">Supprimer</button>`;
+      /* On crée un select HTML pour que l'utilisateur puisse choisir la quantité d'appareil qu'il veut */
+      // document.createElement("select")
+
+      /* On place notre article à l'intérieur de notre section */
+      section.appendChild(newArticle);
+    }
+  }
+  /* A l'inverse, si le panier est vide, on indique un message à l'utilisateur + on bloque l'envoie du formulaire */
+  else {
+    creerParagrapheErreur();
+    empecherEnvoiFormulaire();
+  }
+  
+  /* Modifier le prix d'une ligne si on augmente la quantité du produit voulu : */
+  let quantite = document.querySelectorAll(".quantite");
+  for (all of quantite){
+    all.addEventListener("click", function(e){
+      /* On récupère la valeur de l'input */
+      let valeurInput = e.target.value;
+      /* On récupère le span indiquant le total */
+      let spanMontantArticle = e.target.parentNode.querySelector(".montant");
+      /* On récupère le textContent de ce span */
+      let spanValeurMontantArticle = spanMontantArticle.textContent.slice(0, -2);
+      let montantTotalArticle = parseInt(spanValeurMontantArticle) * valeurInput;
+      let spanTotalArticle = e.target.parentNode.querySelector(".total");
+      spanTotalArticle.textContent = montantTotalArticle + " €";
+    });
   }
 
   /* Supprimer un produit du panier : */
 
-  /* On récupère le bouton supprimer */
-  let supprimerProduit = document.querySelector('.supprimer');
+  /* On récupère les boutons supprimer */
+  let supprimerProduit = document.querySelectorAll('.supprimer');
 
-  function supprimerProduitDuPanier() {
-    supprimerProduit.parentNode.parentNode.remove();
+  for (all of supprimerProduit){
+    all.addEventListener("click", function(e){
+      /* On supprime le produit du localStorage pour ne pas qu'il soit réaffiché si l'utilisateur rafraîchit la page */
+
+      /* On récupère le nom du produit dans la balise article */
+      let titreArticle = e.target.parentNode.querySelector("h2").textContent;
+      /* On boucle sur les entrées du tableau panier */
+      for(all of panier){
+        /* Si la valeur de la variable titreArticle (récupérée plus haut) match celle d'un élément du tableau, alors : */
+        if(titreArticle === all.name){
+          /* On retire uniquement cette entrée du tableau */
+          panier.splice(all, 1);
+          /* On envoie la nouvelle valeur du tableau dans le localStorage (pour ne pas que le produit se réaffiche au moment d'actualiser la page) */
+          localStorage.setItem("panier", JSON.stringify(panier));
+          /* Si le panier est vide, alors on supprime le panier stocké dans le localStorage + on indique à l'utilisateur sur la page que son panier est vide */
+          if(panier.length === 0){
+            localStorage.removeItem("panier");
+            creerParagrapheErreur();
+            empecherEnvoiFormulaire();
+          }
+        }
+      }
+
+      
+      /* On supprime l'article produit de la page panier */
+      e.target.parentNode.remove();
+    });
   }
-  supprimerProduit.addEventListener("click", supprimerProduitDuPanier);
+
+  
 }
